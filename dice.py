e39d34d5154b698fde67579816cb8e9b727147e7
levara <hrvoje.leventic@gmail.com>
2020-10-14 11:50:46 +0200

    Add support for N-sided dices

--- dice.py
+++ dice.py
@@ -3,14 +3,19 @@ import random
 import os,sys
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
         print("Wrong number of arguments!")
         return 1
-    else:
+    elif len(sys.argv) == 2:
         num_dices = int(sys.argv[1])
-        for dice in range(num_dices):
-            roll = random.randint(1,6)
-            print(f'Dice [{dice+1}] roll is: {roll}')
+        num_sides = 6
+    elif len(sys.argv) == 3:
+        num_dices = int(sys.argv[1])
+        num_sides = int(sys.argv[2])
+
+    for dice in range(num_dices):
+        roll = random.randint(1,num_sides)
+        print(f'Dice [{dice+1}] roll is: {roll}')
 
 
 if __name__ == "__main__":
